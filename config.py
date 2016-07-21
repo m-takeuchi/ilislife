8a8362699f2270cba1bc98d1fb285b8d3e0aa3ca
m-takeuchi <m2takeuchi@gmail.com>
2016-07-21 21:56:16 +0900

    Make sequence list independent py file

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+### Caution: Python is sensitive for indentation. Do use "Space" insted of "Tab".
+
 dV = 50 # (V) Minimum volgage step, which must be a dvisor for holding voltages.
 dt_meas = 1 #(s) measurement interval
 dt_op = 1 # (s) time per step for Ve change
@@ -5,7 +7,7 @@ dt_op = 1 # (s) time per step for Ve change
 
 # List of [Voltage (V), holding time(s)]
 DT = 300 # (s) time per step for time-dependence measurement
-SEQ = [ [3000, DT],\
+SEQ = [[3000, DT],\
         [3250, DT],\
         [3500, DT],\
         [3750, DT],\
