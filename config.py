1a3b5627e24bb064a4d035714e54bbb23b015d1b
nanoproc <m2takeuchi@gmail.com>
2016-07-27 11:15:11 +0900

    first long time test on linux

--- config.py
+++ config.py
@@ -6,15 +6,36 @@ dt_op = 1 # (s) time per step for Ve change
 
 
 # List of [Voltage (V), holding time(s)]
-DT = 300 # (s) time per step for time-dependence measurement
-SEQ = [#[3000, DT],\
-       #[3250, DT],\
-       #[3500, DT],\
-       #[3750, DT],\
-       #[4000, DT],\
-       #[4250, DT],\
+DT = 1800 # (s) time per step for time-dependence measurement
+    
+SEQ = [[2000, DT],\
+        [2100, DT],\
+        [2200, DT],\
+        [2300, DT],\
+        [2400, DT],\
+        [2500, DT],\
+        [2600, DT],\
+        [2700, DT],\
+        [2800, DT],\
+        [2900, DT],\
+        [3000, DT],\
+        [3100, DT],\
+        [3200, DT],\
+        [3300, DT],\
+        [3400, DT],\
+        [3500, DT],\
+        [3600, DT],\
+        [3700, DT],\
+        [3800, DT],\
+        [3900, DT],\
+        [4000, DT],\
+        [4100, DT],\
+        [4200, DT],\
+        [4300, DT],\
+        [4400, DT],\
         [4500, DT],\
-        [4750, DT],\
-	[5000, DT],\
-	[5250, DT],\
-        [5500, 3600]]
+        [4600, DT],\
+        [4700, DT],\
+        [4800, DT],\
+        [4900, DT],\
+        [5000, 360000]]
